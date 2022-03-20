8c597a532ec7d5ad289112fe78778a71904a799e
Clockwork-Rat <isaiahraspet@gmail.com>
2022-03-20 13:52:15 -0700

    initial server commit

---
+++ Source/Server/GGServer/GGServer/Instruction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace GGServer
+{
+    public struct Instruction
+    {
+        public string IP { get; private set; }
+        public byte[] body { get; private set; }
+
+        public Instruction(string IP, byte[] body)
+        {
+            this.IP = IP;
+            this.body = body;
+        }
+    }
+}
---
+++ Source/Server/GGServer/GGServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+namespace GGServer
+{
+    class MainClass
+    {
+        static Queue<Instruction> Insts;
+        public static void Main(string[] args)
+        {
+            insts = new Queue<Instruction>();
+
+            Thread update = new Thread(update);
+            update.Start();
+
+            for(; ; )
+            {
+                if(Insts.Count > 0)
+                {
+                    exec(Insts.Dequeue());
+                }
+            }
+        }
+
+        static void update()
+        {
+            
+        }
+
+        static void exec(Instruction i)
+        {
+            
+        }
+    }
+}
