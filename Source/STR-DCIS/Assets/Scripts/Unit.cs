329895ecdada7546e7cf56d015d5dc4387dbebec
Jaron Bauers <jaron.bauers@gmail.com>
2022-04-16 23:01:48 -0700

    fix: fixed unit-tests

--- Source/STR-DCIS/Assets/Scripts/Unit.cs
+++ Source/STR-DCIS/Assets/Scripts/Unit.cs
@@ -4,30 +4,41 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
- public string unitName;
- public int unitLevel;
+    public string unitName;
+    public int unitLevel;
 
- public int damage;
- public int maxHP;
- public int currentHP;  
+    public int damage;
+    public int maxHP;
+    public int currentHP;  
 
- public bool TakeDamage(int dmg)
- {
-  currentHP -= dmg;
+    public bool TakeDamage(int dmg)
+    {
+        currentHP -= dmg;
+        return HealthCalc.DamageCalculator(currentHP);
+    }
 
-  if(currentHP <= 0)
-      return true;
-  else
-      return false;
- 
- } 
+    public void Heal(int amount)
+    {
+        currentHP += amount;
+        currentHP = HealthCalc.HealCalculator(currentHP, maxHP);
+    }
+}
 
- public void Heal(int amount)
- {
-  currentHP += amount;
+public static class HealthCalc
+{   
+    public static bool DamageCalculator(int currentHP)
+    {
+        if(currentHP <= 0)
+            return true;
+        else
+            return false;
+    }
 
-  if(currentHP > maxHP)
-      currentHP = maxHP;
-
- }
+    public static int HealCalculator(int currentHP, int maxHP)
+    {
+        if(currentHP > maxHP)
+            return maxHP;
+        else
+            return currentHP;
+    }
 }
